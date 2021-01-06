cfaa4f92576b8f9db1c883a1a8d8a25fb429c5ab
DezWark <dezwark@gmail.com>
2021-01-07 01:51:59 +0500

    nginx -.py

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+gunicorn -b 0.0.0.0:8080 --workers=5 wsgi
 sudo /etc/init.d/nginx restart
 
