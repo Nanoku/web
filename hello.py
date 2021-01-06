9a146e3252339c942b10683dfe3e771a6815878a
DezWark <dezwark@gmail.com>
2021-01-07 02:28:02 +0500

    topns

--- hello.py
+++ hello.py
@@ -3,12 +3,13 @@
 from wsgiref.simple_server import make_server
 from cgi import parse_qs, escape
 
-def wsgi_application(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-Type','text/plain')]    
-    body = ''
+def wsgi_application(environ, start_response):   
+    body = b""
     d = parse_qs(environ['QUERY_STRING'])
     for item in d:
     	body += str(item) + "=" + str(d[item]) + "\n"
-    start_response (status, headers)
-    return [body]
+    start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+    return iter([body])
