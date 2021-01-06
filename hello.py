47005e7e3c2a53834000d6704a7595470da1a853
Nanoku <nanokuvalda@gmail.com>
2021-01-06 21:41:41 +0000

    x

--- hello.py
+++ hello.py
@@ -4,11 +4,12 @@ from wsgiref.simple_server import make_server
 from cgi import parse_qs, escape
 
 def wsgi_application(environ, start_response):   
-    #body = b""
-    #d = parse_qs(environ['QUERY_STRING'])
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    body = b""
+    d = parse_qs(environ['QUERY_STRING'])
+    body = [bytes(str(i) + '=' + str(d[i]) + '\n', 'ascii') for i in d]
     start_response("200 OK", [
             ("Content-Type", "text/plain"),
             ("Content-Length", str(len(body)))
         ])
+    print()
     return body
