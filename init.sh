eb8dd0e196cf8278113ee7a89f4528ce14abd81a
Nanoku <nanokuvalda@gmail.com>
2021-01-06 22:38:53 +0000

    last

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo nginx -c /etc/nginx/sites-enabled/test.conf
 git config --global user.name "Nanoku"
 git config --global user.email "nanokuvalda@gmail.com"
 gunicorn -w 4 -b 0.0.0.0:8080 hello:wsgi_application
