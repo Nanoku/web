d1e0674cd3dc6676cc71423945368588b9a8aada
Nanoku <nanokuvalda@gmail.com>
2021-01-07 22:14:16 +0000

    4kOldTestsgit add *git add *git add *git add *git add *git add *git add *git add *git add *

--- init2.sh
+++ init2.sh
@@ -6,14 +6,17 @@
 #python3 -m pip install -U Django
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo service nginx restart
-git config --global user.name "Nanoku"
-git config --global user.email "nanokuvalda@gmail.com"
-django-admin startproject ask
+sudo service nginx stop
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 cd ask
-python manage.py startapp qa
-cp -f ~/web/views.py ~/web/ask/qa/views.py
-cp -f ~/web/urls.py ~/web/ask/ask/urls.py
+gunicorn -b 0.0.0.0:8000 ask.wsgi
+#git config --global user.name "Nanoku"
+#git config --global user.email "nanokuvalda@gmail.com"
+#django-admin startproject ask
+#cd ask
+#python manage.py startapp qa
+#cp -f ~/web/views.py ~/web/ask/qa/views.py
+#cp -f ~/web/urls.py ~/web/ask/ask/urls.py
 #sudo gunicorn -c /home/box/web/gunicorn-django.conf --access-logfile acc.log --error-logfile err.log ask.wsgi:application
 cd ~/web/
 sudo gunicorn -w 4 -b 0.0.0.0:8080 hello:wsgi_application
