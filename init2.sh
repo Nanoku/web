4d967259d08bc4e71f865883f5a1f47f7c12c81a
Nanoku <nanokuvalda@gmail.com>
2021-01-07 13:53:33 +0000

    back to future

--- init2.sh
+++ init2.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo nginx -c /etc/nginx/sites-enabled/test.conf
-git config --global user.name "Nanoku"
-git config --global user.email "nanokuvalda@gmail.com"
-django-admin startproject ask
-cd ask
-python manage.py startapp qa
-cd ..
-cp -f views.py qa/views.py
-cp -f urls.py ask/urls.py
- sudo gunicorn -c /home/box/web/gunicorn.conf hello:wsgi_application
- sudo gunicorn -c /home/box/web/gunicorn-django.conf ask.wsgi:application
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#sudo rm -rf /etc/nginx/sites-enabled/default
+#sudo nginx -c /etc/nginx/sites-enabled/test.conf
+#git config --global user.name "Nanoku"
+#git config --global user.email "nanokuvalda@gmail.com"
+#django-admin startproject ask
+#cd ask
+#python manage.py startapp qa
+#cd ..
+#cp -f views.py qa/views.py
+#cp -f urls.py ask/urls.py
+sudo gunicorn -c /home/box/web/gunicorn-django.conf ask.wsgi:application
+#sudo gunicorn -w 4 -b 0.0.0.0:8080 hello:wsgi_application
 #sudo cp -f gunicorn1.service /etc/systemd/system/gunicorn1.service
 #sudo systemctl start gunicorn1
 #sudo systemctl enable gunicorn1
